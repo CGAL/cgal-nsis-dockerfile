76c3b01d5a632b01500b3d0f5b4a86af66af2bfe
Philipp Möller <bootsarehax@gmail.com>
2015-07-22 11:36:21 +0200

    Actual Dockerfile and entrypoint

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM monokrome/wine
+
+RUN apt-get update && apt-get install -y winbind nsis && apt-get clean -y
+# ENV NSIS_URL http://prdownloads.sourceforge.net/nsis/nsis-2.46-setup.exe?download
+# RUN wget -q -O nsis.exe $NSIS_URL && rm -rf /tmp/.wine-* && xvfb-run -a wine nsis.exe && rm -rf /tmp/.wine-*
+
+# Install Locate and inetc plugin
+ADD Locate.dll /usr/share/nsis/Plugins/
+ADD Locate.nsh /usr/share/nsis/Include/
+ADD inetc.dll /usr/share/nsis/Plugins/
+ADD ZipDLL.dll /usr/share/nsis/Plugins/
+
+VOLUME ["/mnt/cgal_sources", "/mnt/cgal_release"]
+
+COPY ./docker_entrypoint.sh /
+ENTRYPOINT ["/docker_entrypoint.sh"]
---
+++ docker_entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'cgal_build_installer' ]; then
+    SOURCES_DIR=/mnt/cgal_sources/
+    NUMBERS_DIR=${SOURCES_DIR}/Maintenance/release_building/
+    MAJOR_NUMBER=`cat ${NUMBERS_DIR}/MAJOR_NUMBER`     # 2 digits max
+    MINOR_NUMBER=`cat ${NUMBERS_DIR}/MINOR_NUMBER`     # 2 digits max
+    BUGFIX_NUMBER=`cat ${NUMBERS_DIR}/BUGFIX_NUMBER`   # 1 digit max
+    public_release_name=`cat "${NUMBERS_DIR}/public_release_name"`
+
+    mkdir nsis_release
+    cd nsis_release
+    cp -R /mnt/cgal_release "${public_release_name}"
+    cp ${SOURCES_DIR}/wininst/developer_scripts/* .
+    sed -i -e "/define *CGAL_SRC/,+20 s/CGAL-${MAJOR_NUMBER}.${MINOR_NUMBER}/${public_release_name}/g" script_cgal.nsi
+    makensis "script_cgal.nsi"
+else
+    exec "$@"
+fi
