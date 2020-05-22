cbc85cf842a9bcabc0835330861553e9d1ce3950
Laurent Rineau <laurent.rineau@cgal.org>
2020-05-22 13:40:13 +0200

    Compatibility with recent versions

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 if [ "$1" = 'cgal_build_installer' ]; then
     SOURCES_DIR=/mnt/cgal_sources/
     NUMBERS_DIR=${SOURCES_DIR}/Maintenance/release_building/
-    MAJOR_NUMBER=`cat ${NUMBERS_DIR}/MAJOR_NUMBER`     # 2 digits max
-    MINOR_NUMBER=`cat ${NUMBERS_DIR}/MINOR_NUMBER`     # 2 digits max
-    BUGFIX_NUMBER=`cat ${NUMBERS_DIR}/BUGFIX_NUMBER`   # 1 digit max
-    public_release_name=`cat "${NUMBERS_DIR}/public_release_name"`
-
+    if [ -f ${NUMBERS_DIR}/MAJOR_NUMBER ]; then
+        MAJOR_NUMBER=`cat ${NUMBERS_DIR}/MAJOR_NUMBER`     # 2 digits max
+        MINOR_NUMBER=`cat ${NUMBERS_DIR}/MINOR_NUMBER`     # 2 digits max
+        BUGFIX_NUMBER=`cat ${NUMBERS_DIR}/BUGFIX_NUMBER`   # 1 digit max
+        public_release_name=`cat "${NUMBERS_DIR}/public_release_name"`
+    else
+        eval $(cmake -DCGALCONFIGVERSIONFILE=${SOURCES_DIR}/CGALConfigVersion.cmake -P ${SOURCES_DIR}/Scripts/developer_scripts/create_new_release_evaluate_versions.cmake 2>&1)
+    fi
     mkdir nsis_release
     cd nsis_release
     cp -R /mnt/cgal_release "${public_release_name}"
